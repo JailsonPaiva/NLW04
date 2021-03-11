d2b8b319cd6f36d8ae732c5bb1aaf0925f3a723b
jailson <jailsonp437@gmail.com>
2021-03-10 21:23:46 -0400

    finish project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css"
+
+
+export function Profile(){
+    const {level} = useContext(ChallengesContext);
+
+
+    return(
+        <div className={styles.container}>
+            <img src="https://github.com/papelgold.png" alt="Jailson Paiva"/>
+
+            <div>
+                <strong>
+                    Jailson Paiva
+                </strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
